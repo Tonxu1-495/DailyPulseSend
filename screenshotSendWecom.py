1a883817a2b553a727fd668fb731fa98f183f6d7
TONXU1 <tony.xu@ingka.ikea.com>
2025-04-18 15:14:55 +0800

    update

--- screenshotSendWecom.py
+++ screenshotSendWecom.py
@@ -19,7 +19,7 @@ if os.name == 'nt':
 # 指定文件路径和对应的 webhook URL
 files_and_urls = [
     (r'C:\RPAData\Test-xy.xlsx',
-     'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=741701b2-90e1-40aa-82d4-ea71cb69279e'),
+     'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=b59764ec-ae6e-4d70-a076-2ec634b1f2f5'),
     #添加第二组文件路径&WebHook地址
     #(r'C:\RPAData\Test-xy-111.xlsx',
      #'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=ae7ce51a-24f0-4db4-a1be-88d6eaad8883')
