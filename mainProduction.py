a156ae6f092a965fa8d8d8a7b4f896006b1617a4
TONXU1 <tony.xu@ingka.ikea.com>
2025-04-29 11:33:14 +0800

    update

---
+++ mainProduction.py
@@ -0,0 +1,51 @@
+import subprocess
+import sys
+import io
+
+# 设置 stdout 编码为 utf-8
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8',errors='ignore')
+
+def run_script(script_name, log_file):
+    try:
+        result = subprocess.run(
+            [r'C:\Users\l-rpa-a-retcn568\Downloads\RPA\DailyPulseSend\.venv\Scripts\python.exe', script_name],
+            capture_output=True, text=True
+        )
+
+        # 打印和记录脚本输出
+        if result.stdout:
+            print(result.stdout, flush=True)
+            log_file.write(f"Output from {script_name}:\n{result.stdout}\n")
+
+        # 检查是否有 stderr 输出
+        if result.stderr:
+            print("Error:", result.stderr, flush=True)
+            # 将 stderr 输出记录到同一个日志文件
+            log_file.write(f"Error in {script_name}:\n{result.stderr}\n")
+
+        # 检查返回码
+        if result.returncode != 0:
+            log_file.write(f"Script {script_name} failed with return code {result.returncode}\n")
+
+    except FileNotFoundError:
+        error_message = f"File not found: {script_name}\n"
+        log_file.write(error_message)
+        print(error_message, flush=True)
+    except Exception as e:
+        error_message = f"An unexpected error occurred while running {script_name}: {str(e)}\n"
+        log_file.write(error_message)
+        print(error_message, flush=True)
+
+if __name__ == "__main__":
+    # 打开日志文件
+    with open('log.txt', 'w', encoding='utf-8') as log_file:
+        script_paths = [
+            r'C:\Users\l-rpa-a-retcn568\Downloads\RPA\DailyPulseSend\callApi.py',
+            r'C:\Users\l-rpa-a-retcn568\Downloads\RPA\GitHub\DailyPulseSend\excelFilterSto401.py',
+            r'C:\Users\l-rpa-a-retcn568\Downloads\RPA\GitHub\DailyPulseSend\excelFilterSto401-2.py',
+            r'C:\Users\l-rpa-a-retcn568\Downloads\RPA\GitHub\DailyPulseSend\batchProcessingScreenshots.py'
+        ]
+
+        # 运行每个脚本并捕获输出和错误
+        for script in script_paths:
+            run_script(script, log_file)
